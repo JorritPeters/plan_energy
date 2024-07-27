c51ccdde9afc4cece5b86e68982e9ae34ecd22b5
JorritPeters <jorritp@gmail.com>
2024-07-27 19:23:17 +0200

    better UI

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import datetime
 import streamlit as st
 
+
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1) 
 today_string = today.strftime('%Y-%m-%d')
@@ -31,7 +32,8 @@ def find_best_time(df, consecutive_hours_needed, max_end_date, max_end_hour):
     df['start_time_rolling_sum'] = df['hour'].shift(consecutive_hours_needed-1)
     df['start_date_rolling_sum'] = df['date'].shift(consecutive_hours_needed-1)
     df_best_hour = df[df.rolling_sum.min() == df.rolling_sum]
-    return df_best_hour
+    average = df['price_sek'].mean()
+    return df_best_hour, average
 
 def get_current_prices():
     df_all_prices = pd.concat([get_energy_prices(today),(get_energy_prices(tomorrow))])
@@ -39,7 +41,19 @@ def get_current_prices():
     return df_all_prices
 
 def plan_appliances(df_all_prices,hours: int = 1, max_end_hour: int = 24 , max_end_date: str = 'NA'):
-    df_return = find_best_time(df_all_prices, hours,tomorrow_string, max_end_hour) 
+    df_return, average = find_best_time(df_all_prices, hours,tomorrow_string, max_end_hour) 
+    # Create three columns
+    col1, col2, col3 = st.columns(3)
+
+    # Display metrics in each column
+    with col1:
+        st.metric(label="Best time to start", value=str(int(df_return.start_time_rolling_sum.values[0])) + ':00')
+
+    with col2:
+        st.metric(label="Average price in SEK", value= (float(df_return.rolling_sum.values[0])))
+
+    with col3:
+        st.metric(label="% better than average", value= str(round(((average - (df_return.rolling_sum.values[0])) / average),2)) + '%')
     return "Start at hour " + str(int(df_return.start_time_rolling_sum.values[0])) + " for optimal prices in the next " + str(hours) + " hours"
     
 
@@ -79,11 +93,17 @@ def main():
     df_all_prices = get_current_prices()
     # Output block
     if consecutive_hours and max_end_time:
-        st.write(f"You have chosen the following input:")
-        st.write(f"- Consecutive Hours: {consecutive_hours}")
-        st.write(f"- Max End Time: {max_end_time}")
+        #st.write(f"You have chosen the following input:")
+        #st.write(f"- Consecutive Hours: {consecutive_hours}")
+        #st.write(f"- Max End Time: {max_end_time}")
         st.write(plan_appliances(df_all_prices,consecutive_hours, max_end_time))
-
+    
         st.bar_chart(df_all_prices, x="datetime", y="price_sek")
+    total_users = 1234567
+    active_users = 987654
+    new_users = 321098
+
+
+
 if __name__ == "__main__":
     main()
